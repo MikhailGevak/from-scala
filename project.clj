3a97c26dae85e83ec7c866274adf303776643435
Tobias Kortkamp <tobias.kortkamp@gmail.com>
2015-01-14 20:44:45 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :profiles {:scala2.10 {:dependencies [[org.scala-lang/scala-library "2.10.4"]]}
              :dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.scala-lang/scala-library "2.11.2"]
+                                  [org.scala-lang/scala-library "2.11.5"]
                                   [midje "1.6.3"]]
                    :plugins [[lein-midje-doc "0.0.24"]
                              [lein-midje "3.1.3"]]}}
@@ -18,5 +18,5 @@
   :dependencies [[cats "0.2.0"
                   :exclusions [com.keminglabs/cljx
                                org.clojure/clojurescript]]
-                 [potemkin "0.3.8"]]
+                 [potemkin "0.3.11"]]
   :aliases {"test-all" ["do" "midje," "with-profile" "+scala2.10" "midje"]})
